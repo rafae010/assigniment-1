48802f3f339dcfc3f4ccdf411dffae889f83ac75
rafae010 <rafaekhan47@gmail.com>
2025-08-30 20:26:56 +0530

    assignment 1

---
+++ ASSIGNMENT 1.py
@@ -0,0 +1,24 @@
+
+'''A=input("ENTER THE FRST NUMBER:")
+B=input("ENTER THE LAST NUMBER:")
+A= int(A)
+B = int(B)
+a=(A+B)
+s=(A-B)
+d=(A/B)
+m=(A*B)
+print("ADDITION:",a)
+print("SUBTRACTION:",s)
+print("MULTIPLICATION:",m)
+print("DIVISION:",d)
+'''
+
+a=input("Enter your first name : ")
+b=input("Enter your last name :  ")
+f=(a + b)
+print("HELLO," ,f ,"! welcome to the python program." )
+
+
+
+
+
